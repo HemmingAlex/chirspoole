6c9af9742c3414f80630104360a5f30c82a1fa3a
Alex <alexander.hemming@gmail.com>
2025-02-12 15:21:06 +0000

    layour

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import Socials from "../components/SocialBar ";
 import { Lato } from "next/font/google";
 // import { Menu } from "lucide-react";
 import MobileNavigation from "../components/MobileNavigation";
+import { usePathname } from 'next/navigation'
 
 const lato = Lato({
   subsets: ["latin"],
@@ -21,7 +22,8 @@ export default function RootLayout({
 }>) {
   const [isScrolled, setIsScrolled] = useState(false);
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-
+  const pathname = usePathname();
+  const isHomePage = pathname === '/';
   useEffect(() => {
     const handleScroll = () => {
       setIsScrolled(window.scrollY > window.innerHeight);
@@ -53,20 +55,21 @@ export default function RootLayout({
         <SocialIcons />
         <nav
           className={`fixed top-0 w-full z-50 transition-colors duration-300 ${
-            isScrolled ? "bg-white wblack shadow-md" : "bg-transparent wwhite"
+            isScrolled || !isHomePage ? "bg-white wblack shadow-md" : "bg-transparent wwhite"
           }`}
         >
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex justify-between h-20 items-center">
               <div className="flex-shrink-0">
                 <Link href="/" className="wxl font-bold ">
-                  {!isScrolled ? (
-                    <img
-                      src="/assets/extract/w/Shades_music_logo_newhat_0225HDfinal_whitetrans.svg"
-                      alt="Band performance"
-                      className="h-24 py-2"
-                    />
+                  {!isScrolled && isHomePage ? (
+                    <></>
                   ) : (
+                    // <img
+                    //   src="/assets/extract/w/Shades_music_logo_newhat_0225HDfinal_whitetrans.svg"
+                    //   alt="Band performance"
+                    //   className="h-24 py-2"
+                    // />
                     <img
                       src="/assets/extract/w/Shades_music_logo_newhat_0225HDfinal_Blktrans.svg"
                       alt="Band performance"
@@ -124,12 +127,12 @@ export default function RootLayout({
                 <button
                   onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
                   className={` ${
-                    isScrolled ? "text-black" : "text-white"
+                    isScrolled || !isHomePage ? "text-black" : "text-white"
                   } p-2 rounded-md hover:bg-gray-700/50 transition-colors`}
                   aria-label="Toggle menu"
                 >
                   {
-                    isMobileMenuOpen ? <div />:<>Menu</>
+                    isMobileMenuOpen ? <div /> : <>Menu</>
                     // #<Menu className="h-6 w-6" />
                   }
                 </button>
